814ea5787fdbec6b56de824718b317046906886a
FlorinCristianGrad7 <florin.grad@student.unitbv.ro>
2023-04-27 23:08:39 +0300

    added the project imports

--- test.py
+++ test.py
@@ -8,3 +8,8 @@ frameHeight = 480
 brightness = 180
 threshold = 0.75
 font = cv2.FONT_HERSHEY_SIMPLEX
+
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, brightness)
